402e4d1c10c6e5b4b7254a4c3276cf3b348ea524
CORP\STHEIJDE <stefan.heijden@capgemini.com>
2022-04-22 15:07:44 +0200

    Clean up and adding JCR path as Inner File Name

--- src/main/java/hello/YAMLReader.java
+++ src/main/java/hello/YAMLReader.java
@@ -8,11 +8,13 @@ import java.nio.file.Path;
 
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 class YAMLReader {
     public static final int SPACES_PER_TAB = 2;
     public static final String FOLDER_TYPE = "jcr:primaryType: hippostd:folder";
+    public static final String ROOT_JCR_PATH = "/content/documents/netherlandsandyou/";
     private final Path startPath;
     private List<String> lines;
     private final List<String> savedLines;
@@ -73,34 +75,42 @@ class YAMLReader {
             if (writePath.getNameCount() < startPath.getNameCount()) {
                 throw new Exception("Tried writing to root folder!");
             }
-            Files.createDirectories(Paths.get(writePath + "\\" + getCurrentFileName()));
+            Files.createDirectories(Paths.get(writePath + "\\" + getCurrentFileName(savedLines.get(0))));
         } catch (IOException e) {
             System.out.println("Error creating directory at " + writePath + "\n" + e.getMessage());
         }
     }
     public void createYAML() {
-        try(FileWriter writer = new FileWriter(Paths.get(writePath +  "\\" + getCurrentFileName() + ".yaml").toFile())) {
-            for(String str: savedLines) {
-                if (str.length() < depth * SPACES_PER_TAB) {
-                    writer.write(str + System.lineSeparator());
-                } else {
-                    writer.write(str.substring(depth * SPACES_PER_TAB) + System.lineSeparator());
-                }
+        try(FileWriter writer = new FileWriter(Paths.get(writePath +  "\\" + getCurrentFileName(savedLines.get(0)) + ".yaml").toFile())) {
+            Iterator<String> iterator = savedLines.iterator();
+            writer.write(getInnerFileName(iterator.next()) + System.lineSeparator());
+            while(iterator.hasNext()) {
+                String str = iterator.next();
+                str = str.length() < depth * SPACES_PER_TAB ? str : str.substring(depth * SPACES_PER_TAB);
+                writer.write(str + System.lineSeparator());
             }
         } catch (IOException e) {
-            System.out.println("Error creating YAML file at " + writePath +  "/" + getCurrentFileName() + ".yaml" + "\n" + e.getMessage());
+            System.out.println("Error creating YAML file at " + writePath +  "/" + getCurrentFileName(savedLines.get(0)) + ".yaml" + "\n" + e.getMessage());
         }
-        writePath = Paths.get(String.format("%s\\%s", writePath, getCurrentFileName()));
+        writePath = Paths.get(String.format("%s\\%s", writePath, getCurrentFileName(savedLines.get(0))));
         duplicate = false;
         savedLines.clear();
     }
 
-    private String getCurrentFileName() {
-        String trimmedName = savedLines.get(0).trim();
+    private String getCurrentFileName(String name) {
+        String trimmedName = name.trim();
         if(duplicate) {
             return trimmedName.substring(3, trimmedName.length() - 1);
         } else {
             return trimmedName.substring(1, trimmedName.length() - 1);
         }
+    }
+
+    private String getInnerFileName(String name) {
+        String fileName = getCurrentFileName(name);
+        String subPath = startPath.getNameCount() < writePath.getNameCount() ?
+                writePath.subpath(startPath.getNameCount(), writePath.getNameCount()).toString() + "/":
+                "";
+        return ROOT_JCR_PATH + subPath + fileName + ":";
     }
 }
