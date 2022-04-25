51132a9f543628d4c923066ddea9a05d214c1e7a
CORP\STHEIJDE <stefan.heijden@capgemini.com>
2022-04-25 08:44:59 +0200

    Added Elastic Export Processor

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,6 +0,0 @@
-
-:
-
-.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
-W
-'src/main/java/hello/CommandMapping.java,a\6\a6159335ae36c72a6aed8b5a54796c7eaa11fa0e
---
+++ src/main/java/hello/ElasticExportProcessor.java
@@ -0,0 +1,86 @@
+package hello;
+
+import java.nio.file.*;
+import java.io.*;
+import java.util.*;
+import static java.util.stream.Collectors.*;
+
+public class ElasticExportProcessor {
+    List<String> lines;
+    File targetFile;
+
+    public ElasticExportProcessor(String resourcePath, String targetPath) {
+        try {
+            lines = Files.readAllLines(Paths.get(resourcePath));
+            createNewFile(targetPath);
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+      }
+    }
+
+    public ElasticExportProcessor replace(final String from, final String to){
+        lines = lines
+                .stream()
+                .map(e -> e.replace(from, to))
+                .collect(toList());
+        return this;
+    }
+
+    public ElasticExportProcessor makeDistinct(){
+        lines = lines
+                .stream()
+                .distinct()
+                .collect(toList());
+        return this;
+    }
+
+    public ElasticExportProcessor getSubstring(final int start, final int end) {
+        lines = lines
+                .stream()
+                .map(e -> e.substring(start, e.length() - end))
+                .collect(toList());
+        return this;
+    }
+
+    public ElasticExportProcessor scimJSONRequest(String JSON_var){
+        boolean saveNextLine = false;
+	    List<String> savedLines = new ArrayList();
+        for(String line : lines) {
+            if(saveNextLine) {
+		String newLine = line.trim();
+                savedLines.add(newLine.substring(1, newLine.length() - 1));
+                saveNextLine = false;
+            }
+            if(line.contains(JSON_var)) {
+              saveNextLine = true;
+            }
+        }
+        lines = savedLines;
+        return this;
+    }
+
+    public void start() throws IOException {
+        FileWriter fw = new FileWriter(targetFile);
+        for (String line : lines) {
+            fw.write(line + "\n");
+        }
+        fw.close();
+    }
+
+    private void createNewFile(String pathName) {
+        try {
+            targetFile = new File(pathName);
+            if (targetFile.createNewFile()) {
+                System.out.println("File created: " + targetFile.getName());
+            } else {
+                System.out.println("File already exists. Clear content");
+                PrintWriter writer = new PrintWriter(targetFile);
+                writer.print("");
+                writer.close();
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
