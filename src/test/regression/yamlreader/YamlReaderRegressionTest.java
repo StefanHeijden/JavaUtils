a73b42b69324163772a849c68a7ddee90411de8f
CORP\STHEIJDE <stefan.heijden@capgemini.com>
2022-05-30 16:36:49 +0200

    Added regressiontest

--- src/test/regression/yamlreader/YamlReaderRegressionTest.java
+++ src/test/regression/yamlreader/YamlReaderRegressionTest.java
@@ -1,38 +1,28 @@
-package test.junit.utilities;
+package test.regression.yamlreader;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import utilities.StartClass;
 import utilities.UserInterface;
 
 @ExtendWith(MockitoExtension.class)
-class StartClassTest {
+class YamlReaderRegressionTest {
 
-    @org.junit.jupiter.api.Test
-    void startup() {
-        try {
-            try (MockedStatic<UserInterface> utilities = Mockito.mockStatic(UserInterface.class)) {
-                utilities.when(() -> UserInterface.getUserInput(Mockito.anyString()))
-                        .thenReturn("exit");
-                StartClass.init();
-                StartClass.start();
-            }
-        } catch (Exception e) {
-            Assertions.fail();
-        }
-        Assertions.assertTrue(true);
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+        // TODO clear results
     }
 
     @org.junit.jupiter.api.Test
-    void printHelp() {
+    void main() {
         try {
             try (MockedStatic<UserInterface> utilities = Mockito.mockStatic(UserInterface.class)) {
                 utilities.when(() -> UserInterface.getUserInput(Mockito.anyString()))
-                        .thenReturn("help")
+                        .thenReturn("cd C:/Users/stheijde/Repositories/Java/java-hello-world-with-maven-master/src/test/data/yaml")
+                        .thenReturn("yaml landen ./../../results")
                         .thenReturn("exit");
                 StartClass.init();
                 StartClass.start();
@@ -40,7 +30,7 @@ class StartClassTest {
         } catch (Exception e) {
             Assertions.fail();
         }
-        //TODO check input screen whether help was printed
+        //TODO check results
         Assertions.assertTrue(true);
     }
 }
